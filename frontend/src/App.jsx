865829225b52118a3f656e5efd07aa5ef60f75df
hanoon-07 <muhhamedhanoon@gmail.com>
2025-01-17 10:52:06 +0530

    base-button-ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,7 @@ const App = () => {
     return (
         <Provider store={store}>
             <div>
-                <h1 className='text-slate-800 font-extrabold m-10 text-[3rem]'>Test</h1> 
-                <Button label="run" Icon={Camera} iconStyle={{size:15,className:"text-textGray"}}></Button>
+                
             </div>
         </Provider>
     )
