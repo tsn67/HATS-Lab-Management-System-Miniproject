5b0032dd69edc1cbbc4d37ec431559a0f5778678
tsn67 <tomsebastian67@gmail.com>
2025-01-17 09:51:47 +0530

    added colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDark: '#000000',
+        textGray: '#9F8F8F',
+        textRed: '#F6664C',
+        backgroundRed: '#322525',
+        textGreen: '#45BF72',
+        textBlue: '#4D69F0',
+        backgroundBlue: '#2A3A47',
+        buttonGray: '#403E3E',
+        secondaryGray: '#333333',
+        darkGray: '#262626',
+        
+      }
+    },
   },
   plugins: [],
 }
